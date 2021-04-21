6cbbfdbbe8f1e956557ead496cf7cdfd5e969733
Aditya Bhatia <17334246+adityacb15@users.noreply.github.com>
2021-04-21 13:52:28 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,8 +1,7 @@
 import sys
 
-# Define a main() function that prints a little greeting.
+
 def main():
-  # Get the name from the command line, using 'World' as a fallback.
   if len(sys.argv) >= 2:
     name = sys.argv[1]
   else:
